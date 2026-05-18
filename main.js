11151f821883e5a7482ce9e1ebf62344031606cf
LEANDRO DOS REIS FILHO <25203700@CFP127ALUNO.LOCAL>
2026-05-18 17:00:57 -0300

    Feat: importando API

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 'use strict'
 
+async function getDadosProdutos(dados){
+    const url = `https://fakestoreapi.com/${dados}`
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
